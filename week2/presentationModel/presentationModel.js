99fa34cbb0704a664e7d9959304e58d0699cd9e0
Kerkmann <felix.kerkmann@s-p-m.ch>
2020-09-21 16:27:20 +0200

    vorlesung webcl woche 2

--- week2/presentationModel/presentationModel.js
+++ week2/presentationModel/presentationModel.js
@@ -10,7 +10,15 @@ const Attribute = value => {
     const validObs = Observable(true);
 
     // todo: add required functions here
+    let   converter = id;
+    const setConverter = newConverter => {
+        converter = newConverter;
+        valueObs.setValue(converter(valueObs.getValue()));
+    }
+    const setConvertedValue = value => valueObs.setValue(converter(value));
+
+    const setValidator = validator => valueObs.onChange(val => validObs.setValue(validator(val)));
 
 
-    return { valueObs, validObs }
+    return { valueObs, validObs, setConverter, setValidator, setConvertedValue }
 };
